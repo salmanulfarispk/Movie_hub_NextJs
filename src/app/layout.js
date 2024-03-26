53a14c9aa3e5ce969959e1e20b536d815c22e25d
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-03-26 12:09:32 +0530

    add search box

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import { Providers } from "./Providers";
 import Navbar from "@/components/Navbar";
+import { Searchbox } from "@/components/Searchbox";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
           
         <Header/>
         <Navbar/>
+        <Searchbox/>
         {children}
 
         </Providers>
