98ba10c6a33e253f703aba526f686058007f0e47
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-03-26 09:25:33 +0530

    fetch datas

---
+++ src/components/Results.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Results({ results }) {
+  return (
+    <div>
+   {
+    results.map((results)=>(
+
+    <div key={results}>
+
+
+    ))
+   }
+
+
+    </div>
+  )
+}
